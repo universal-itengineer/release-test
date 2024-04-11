26e439171f0c698039da278447b972a51a16d781
Nikita Korolev <141920865+universal-itengineer@users.noreply.github.com>
2024-04-11 13:49:16 +0300

    feat: run script.sh (#16)
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

---
+++ run.sh
@@ -0,0 +1,8 @@
+NEW_BRANCH=feat/new-feat7
+COMMIT_MSG=$(echo $NEW_BRANCH | cut -d '/' -f2)
+git swich main
+git pull
+git checkout -b $NEW_BRANCH
+git echo $NEW_BRANCH >> awesome.txt
+git add . && git commit -m "feat: $COMMIT_MSG" --signoff
+git push --set-upstream origin $NEW_BRANCH
