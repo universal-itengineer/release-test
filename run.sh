bd1af9f456a15a36bd465c8e0d80d112d7478c84
Nikita Korolev <141920865+universal-itengineer@users.noreply.github.com>
2024-09-19 11:07:08 +0300

    feat: new feat9 (#19)
    * feat: new-feat8
    
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>
    
    * feat: new-feat9
    
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>
    
    ---------
    
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-NEW_BRANCH=feat/new-feat7
+NEW_BRANCH=feat/new-feat9
 COMMIT_MSG=$(echo $NEW_BRANCH | cut -d '/' -f2)
-git swich main
-git pull
+# git swich main
+# git pull
 git checkout -b $NEW_BRANCH
 git echo $NEW_BRANCH >> awesome.txt
 git add . && git commit -m "feat: $COMMIT_MSG" --signoff
